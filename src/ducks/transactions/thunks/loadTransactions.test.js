09c5048d4daa1a55e5ba63d284aaae1ca0ebe8a8
Marcin Mazurek <marcin@mazurek.pro>
2018-11-22 23:27:39 +0100

    Promises -> async

--- src/ducks/transactions/thunks/loadTransactions.test.js
+++ src/ducks/transactions/thunks/loadTransactions.test.js
@@ -5,32 +5,28 @@ import mockHttpRequest from '../../../../test-utils/mockHttpRequest';
 
 import TRANSACTIONS from '../../../../test-data/transactions';
 
-test('handles saving transaction to the API and indicates it with LOAD_TRANSACTIONS_REQUEST and LOAD_TRANSACTIONS_SUCCESS actions', () => {
-  expect.assertions(3);
-
+test('handles loading transaction from the API and indicates it with LOAD_TRANSACTIONS_REQUEST and LOAD_TRANSACTIONS_SUCCESS actions', async () => {
   mockHttpRequest({
     url: '/transactions',
     responseBody: TRANSACTIONS,
   });
 
-  return runThunk(loadTransactions).then(dispatch => {
-    expect(dispatch).toBeCalledWith(loadTransactionsRequest());
-    expect(dispatch).toBeCalledWith(loadTransactionsSuccess(TRANSACTIONS));
-    expect(dispatch).toHaveBeenCalledTimes(2);
-  });
+  const dispatchMock = await runThunk(loadTransactions);
+
+  expect(dispatchMock).toBeCalledWith(loadTransactionsRequest());
+  expect(dispatchMock).toBeCalledWith(loadTransactionsSuccess(TRANSACTIONS));
+  expect(dispatchMock).toHaveBeenCalledTimes(2);
 });
 
-test('handles failure and indicates it with LOAD_TRANSACTIONS_REQUEST and LOAD_TRANSACTIONS_ERROR actions', () => {
-  expect.assertions(3);
-
+test('handles failure and indicates it with LOAD_TRANSACTIONS_REQUEST and LOAD_TRANSACTIONS_ERROR actions', async () => {
   mockHttpRequest({
     url: '/transactions',
     responseCode: 500,
   });
 
-  return runThunk(loadTransactions).then(dispatch => {
-    expect(dispatch).toBeCalledWith(loadTransactionsRequest());
-    expect(dispatch).toBeCalledWith(loadTransactionsError());
-    expect(dispatch).toHaveBeenCalledTimes(2);
-  });
+  const dispatchMock = await runThunk(loadTransactions);
+
+  expect(dispatchMock).toBeCalledWith(loadTransactionsRequest());
+  expect(dispatchMock).toBeCalledWith(loadTransactionsError());
+  expect(dispatchMock).toHaveBeenCalledTimes(2);
 });
