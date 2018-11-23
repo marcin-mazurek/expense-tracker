887d62548f298b617a8cf4bd8a9d26c7b1b660a2
Marcin Mazurek <marcin@mazurek.pro>
2018-11-23 11:58:08 +0100

    Update TransactionListContainer.test.js

--- src/components/TransactionList/TransactionListContainer.test.js
+++ src/components/TransactionList/TransactionListContainer.test.js
@@ -1,19 +1,41 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
 import mockHttpRequest from '../../../test-utils/mockHttpRequest';
 import TRANSACTIONS from '../../../test-data/transactions';
 import TransactionListContainerPOM from '../../../test-poms/TransactionListContainerPOM';
+import TransactionListContainer from './TransactionListContainer';
+import provideStore from '../../model/provideStore';
 import waitUntil from 'wait-until-promise';
 
+async function waitUntilTransactionsLoaded(component) {
+  return await waitUntil(() => {
+    component.update();
+    return component.find('[data-test-element="transaction-item"]').length > 0;
+  });
+}
+
 // TODO: xtest -> test to enable the test
-xtest('loads transactions from API', async () => {
-  // TODO: Prepare network request mock - mock GET request to /transactions and respond with TRANSACTIONS object
+test('loads transactions from API', async () => {
+  mockHttpRequest({
+    url: '/transactions',
+    responseBody: TRANSACTIONS,
+  });
 
-  const component = new TransactionListContainerPOM();
-  await waitUntil(() => component.areTransactionsLoaded());
+  const component = mount(provideStore(<TransactionListContainer />));
 
-  // TODO: Assert that there are two transactions
+  await waitUntilTransactionsLoaded(component);
+
+  // Assert that there are two transactions
+  expect(component.find('[data-test-element="transaction-item"]')).toHaveLength(2);
 
   // Assert that value of 1st transaction matches TRANSACTIONS[0].value
-  expect(component.getTransaction(0).getValue()).toEqual(TRANSACTIONS[0].value.toFixed(2));
+  expect(
+    component.find('[data-test-element="transaction-item"]')
+      .at(0)
+      .find('[data-test-element="transaction-item-value"]')
+      .text()
+    ).toEqual(TRANSACTIONS[0].value.toFixed(2));
 
   // TODO: Assert that category of 1st transaction matches TRANSACTIONS[0].category
 
